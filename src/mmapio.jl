fd617bb2365eab5f46c8f7c8e2f45169104d6300
Simon Kornblith <simon@simonster.com>
2015-08-04 17:09:38 -0400

    Fix on Linux

--- src/mmapio.jl
+++ src/mmapio.jl
@@ -30,7 +30,7 @@ if OS_NAME === :Linux
     grow(io::IOStream, sz::Integer) =
         systemerror("pwrite", ccall(:jl_pwrite, Cssize_t,
                                     (Cint, Ptr{UInt8}, Uint, FileOffset),
-                                    fd(io.f), &UInt8(0), 1, newsz - 1) < 1)
+                                    fd(io), &UInt8(0), 1, newsz - 1) < 1)
 else
     grow(io::IOStream, sz::Integer) = truncate(io, sz)
 end
