b99b767367b621b6d188adac97682bb0a617a82f
Jonas Isensee <jonas.isensee@web.de>
2021-02-14 17:33:11 +0100

    move types for dispatch out front

--- src/data/writing_datatypes.jl
+++ src/data/writing_datatypes.jl
@@ -1,38 +1,7 @@
-const Initialized = Union{Type{Val{true}}, Type{Val{false}}}
-
-const Pointers = Union{Ptr{Cvoid}, IndirectPointer}
-
-struct OnDiskRepresentation{Offsets,JLTypes,H5Types} end
-odr_sizeof(::Nothing) = 0
-@Base.pure odr_sizeof(x::DataType) = Int(x.size)
-
-struct UnknownType{T}
-    name::T
-    parameters::Vector{Any}
-
-    UnknownType{T}(name) where T = new(name)
-    UnknownType{T}(name, parameters) where T = new(name, parameters)
-end
-UnknownType(name) = UnknownType{typeof(name)}(name)
-UnknownType(name, parameters) = UnknownType{typeof(name)}(name, parameters)
-
-struct Vlen{T}
-    size::UInt32
-    id::GlobalHeapID
-end
-odr_sizeof(::Type{T}) where {T<:Vlen} = 4 + sizeof(GlobalHeapID)
-
 # Initial ODR for DataType
 const DataTypeODR = OnDiskRepresentation{(0, odr_sizeof(Vlen{String})),Tuple{String,Vector{Any}},Tuple{Vlen{String},Vlen{RelOffset}}}
 
 const NULL_COMMITTED_DATATYPE = CommittedDatatype(RelOffset(0), 0)
-# Look up the corresponding committed datatype for a given type
-macro lookup_committed(f, T)
-    quote
-        cdt = get($(esc(f)).jlh5type, $(esc(T)), nothing)
-        cdt !== nothing && return cdt::CommittedDatatype
-    end
-end
 
 function track_weakref!(f::JLDFile, header_offset::RelOffset, @nospecialize v)
     header_offset !== NULL_REFERENCE && (f.jloffset[header_offset] = WeakRef(v))
