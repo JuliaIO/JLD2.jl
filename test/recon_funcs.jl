ef9cd3f4f1f851d81fc1bb448855d7b10c33d9a2
Jonas Isensee <jonas.isensee@web.de>
2021-05-16 18:04:32 +0200

    add tests

--- test/recon_funcs.jl
+++ test/recon_funcs.jl
@@ -37,3 +37,104 @@ end
     @test 42 == round_trip(S2(42, λ)).f()
     @test 42 == round_trip(S3(λ)).f()
 end
+
+@testset "Anonymous Functions" begin
+    fn = joinpath(mktempdir(), "test.jld2")
+
+    # "Pure" function
+    f1 = (x) -> x^2
+
+    # Closure
+    f2 = let x=0; () -> (x+=1); end
+    f2(); f2(); # Call twice for good measure
+
+    # Global Ref
+    f3 = () -> global_variable
+
+    jldsave(fn; f1, f2, f3)
+
+    g1 = load(fn, "f1")
+    g2 = load(fn, "f2")
+    g3 = load(fn, "f3")
+
+    @test f1 !== g1
+    @test f1(42) == g1(42)
+
+    @test f2 !== g2
+    @test f2() == g2()
+    # Test again to test mutation of internal state
+    @test f2() == g2()
+
+    @test f3 !== g3
+    @test_throws UndefVarError g3()
+    global global_variable = 2π
+    @test f3() == g3()
+
+    # Now test again in a new julia session
+    @testset "Anonymous Function from different session" begin
+        tmpdir = mktempdir()
+        atexit(() -> rm(tmpdir; force = true, recursive = true))
+
+        saving_filename = joinpath(tmpdir, "saving.jld2")
+        fn = joinpath(tmpdir, "funs.jld2")
+        saving_contents = """
+            append!(Base.LOAD_PATH, $(Base.LOAD_PATH))
+            unique!(Base.LOAD_PATH)
+            using JLD2
+            
+            # "Pure" function
+            f1 = (x) -> sqrt(x)
+    
+            # Closure
+            f2 = let x=""; () -> (x*="a"); end
+            f2(); f2(); # Call twice for good measure
+        
+            # Global Ref
+            f3 = () -> global_variable
+
+            module AnonFunctionModule
+                # "Pure" function
+                f4 = (x) -> sqrt(x)
+        
+                # Closure
+                f5 = let x=""
+                     () -> (x*="a")
+                    end
+                f5(); f5(); # Call twice for good measure
+            
+                # Global Ref
+                f6 = () -> module_variable
+            end
+            using .AnonFunctionModule: f4, f5, f6
+            jldsave("$(fn)"; f1, f2, f3, f4, f5, f6)
+        """
+        open(saving_filename, "w") do io
+            println(io, saving_contents)
+        end
+        saving_cmd = `$(Base.julia_cmd()) $(saving_filename)`
+        @test better_success(saving_cmd)
+
+        f1 = load(fn, "f1")
+        @test f1(4) == 2
+        f2 = load(fn, "f2")
+        @test f2() == "aaa"
+        f3 = load(fn, "f3")
+        @test f3() == global_variable
+        f4 = load(fn, "f4")
+        @test f4(4) ==2 
+        f5 = load(fn, "f5")
+        @test f5() == "aaa"
+        f6 = load(fn, "f6")
+        @test_throws UndefVarError f6()
+        global module_variable = 3
+        @test f6() == module_variable 
+
+        module AnonFunctionModule
+            module_variable = 4
+        end
+        f6 = load(fn, "f6")
+        f6()
+
+    end
+
+end
