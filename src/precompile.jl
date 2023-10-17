21f9d2c141b06a7c5e19d950c01ef9be2d06c0b6
Steve Kelly <kd2cca@gmail.com>
2023-10-17 15:23:06 -0400

    switch to PrecompileTools.jl
    This provides a minor TTFX speedup, for example with the workload
    in src/precompile.jl:
    
    ```
      julia> @time mktemp() do path, _
    
                   # jldsave
                   jldsave(path; a, b, c, d, e, f, g, h)
    
                   jldopen(path) do f
                       for k in keys(f)
                           f[k]
                       end
                   end
    
                   nothing
               end
    ```
    
    Before:
    
    ```
      0.158051 seconds (195.66 k allocations: 13.735 MiB, 33.56% gc time, 95.68% compilation time)
    ```
    
    After:
    
    ```
      0.038961 seconds (24.42 k allocations: 1.921 MiB, 80.58% compilation time)
    ```
    
    The load time/memory is essentially the same:
    
    Before:
    
    ```
      julia> @time using JLD2
      0.102329 seconds (137.15 k allocations: 12.254 MiB)
    ```
    
    After:
    
    ```
      julia> @time using JLD2
      0.106296 seconds (146.97 k allocations: 12.871 MiB)
    ```

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,24 +1,28 @@
-let 
-    mktemp() do path, _
 
-        a=1
-        b=2.0
-        c=[1.0,2.0]
-        d=rand(ComplexF64,100,100)
-        e=Dict(s => 2 for s in ["a", "b", "c"])
-        f="A String"
-        g=ntuple(i->i^2, 5)
-        h=rand(2000)
 
-        # jldsave
-        jldsave(path; a, b, c, d, e, f, g, h)
+@setup_workload begin
+    a=1
+    b=2.0
+    c=[1.0,2.0]
+    d=rand(ComplexF64,100,100)
+    e=Dict(s => 2 for s in ["a", "b", "c"])
+    f="A String"
+    g=ntuple(i->i^2, 5)
+    h=rand(2000)
 
-        jldopen(path) do f
-            for k in keys(f)
-                f[k]
+    @compile_workload begin
+        mktemp() do path, _
+
+            # jldsave
+            jldsave(path; a, b, c, d, e, f, g, h)
+
+            jldopen(path) do f
+                for k in keys(f)
+                    f[k]
+                end
             end
-        end
 
-        nothing
+            nothing
+        end
     end
 end
