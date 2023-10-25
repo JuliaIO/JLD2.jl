8ad18b8ee35dc5d2f3c3f9ef3391702ff5d79e51
Steve Kelly <kd2cca@gmail.com>
2023-10-25 11:44:21 -0400

    Revert "Align writeas for Dict with wconvert"

--- src/data/specialcased_types.jl
+++ src/data/specialcased_types.jl
@@ -246,7 +246,9 @@ rconvert(::Type{Core.SimpleVector}, x::Vector{Any}) = Core.svec(x...)
 
 ## Dicts
 
-writeas(::Type{<:AbstractDict{K,V}}) where {K,V} = Vector{Pair{K,V}}
+writeas(::Type{Dict{K,V}}) where {K,V} = Vector{Pair{K,V}}
+writeas(::Type{IdDict{Any,Any}}) = Vector{Pair{Any,Any}}
+writeas(::Type{Base.ImmutableDict{K,V}}) where {K,V} = Vector{Pair{K,V}}
 wconvert(::Type{Vector{Pair{K,V}}}, x::AbstractDict{K,V}) where {K,V} = collect(x)
 function rconvert(::Type{T}, x::Vector{Pair{K,V}}) where {T<:AbstractDict,K,V}
     d = T()
