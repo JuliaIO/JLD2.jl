930b5ee5438135eb7955d0f11caafd71ec65b72f
余宏彬 <yuhongbin@tongyuan.cc>
2023-12-11 19:05:46 +0800

    update precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -11,22 +11,25 @@
     h=rand(2000)
 
     @compile_workload begin
-
-        # Win7 and below systems use IOStream method to ensure pre-compilation correct
-        if Sys.iswindows() && Sys.windows_version().major <= 6 && Sys.windows_version().minor <= 1
-            jldsave(path, IOStream; a, b, c, d, e, f, g, h)
-            jldopen(path; iotype = IOStream) do f
-                for k in keys(f)
-                    f[k]
+        mktemp() do path, _
+            
+            # Win7 and below systems use IOStream method to ensure pre-compilation correct
+            if Sys.iswindows() && Sys.windows_version().major <= 6 && Sys.windows_version().minor <= 1
+                jldsave(path, IOStream; a, b, c, d, e, f, g, h)
+                jldopen(path; iotype = IOStream) do f
+                    for k in keys(f)
+                        f[k]
+                    end
+                end
+            else
+                jldsave(path; a, b, c, d, e, f, g, h)
+                jldopen(path) do f
+                    for k in keys(f)
+                        f[k]
+                    end
                 end
             end
-        else
-            jldsave(path; a, b, c, d, e, f, g, h)
-            jldopen(path) do f
-                for k in keys(f)
-                    f[k]
-                end
-            end
+            nothing
         end
     end
 end
