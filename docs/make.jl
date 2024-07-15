7d0c9c18a9e993c9d9940deecfc302d8f4ccdd49
Abhro R <5664668+abhro@users.noreply.github.com>
2024-07-15 19:37:25 -0400

    Use Documenter v1 to build docs site

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter
 using JLD2
+using Documenter.Remotes: GitHub
 
 makedocs(;
     modules = [JLD2],
@@ -7,19 +8,20 @@ makedocs(;
     build = "build",
     clean = true,
     doctest = true,
-    #root="./",
-    #src="src",
-    repo = "https://github.com/JuliaIO/JLD2.jl",
+    #root = "./",
+    #src = "src",
+    repo = GitHub("JuliaIO/JLD2.jl"),
     pages = [
         "Basics" => "index.md",
-	"Custom Serialization" => "customserialization.md",
-    "Compression" => "compression.md",
-	"Internals & Design" => "internals.md",
-	"HDF5 Compatibility" => "hdf5compat.md",
-    "Advanced Usage" => "advanced.md",
-    "Legacy" => "legacy.md",
-    	],
-    )
+        "Custom Serialization" => "customserialization.md",
+        "Compression" => "compression.md",
+        "Internals & Design" => "internals.md",
+        "HDF5 Compatibility" => "hdf5compat.md",
+        "Advanced Usage" => "advanced.md",
+        "Legacy" => "legacy.md",
+    ],
+    warnonly = [:cross_references],
+)
 
 deploydocs(
     repo = "github.com/JuliaIO/JLD2.jl.git",
