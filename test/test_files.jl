91dc0036137f2e3a908af2971bfa8a9b9d9a29ad
Jonas Isensee <jonas.isensee@web.de>
2024-07-28 00:18:30 +0200

    move some more tests

--- test/test_files.jl
+++ test/test_files.jl
@@ -24,7 +24,7 @@ update Artifacts.toml:
         lazy=true)
 update artifact string below to reflect new version number
 =#
-testfiles = artifact"testfiles/JLD2TestFiles-0.1.4/artifacts"
+testfiles = artifact"testfiles/JLD2TestFiles-0.1.5/artifacts"
 
 @testset "HDF5 compat test files" begin
     # These are test files copied from the HDF5.jl test suite
@@ -173,4 +173,88 @@ end
     data = load(fn, "test")
     @test propertynames(data) == (:val,)
     @test data.val == 4 
+end
+
+module RecoverableChangesInStructs
+    using JLD2, Test
+    fn = joinpath(Main.testfiles, "recoverable_changes_in structs.jld2")
+    # for saving:
+    # struct A; x::Int; end
+    # struct B; a::A; end
+
+    # struct C; x::Int; end
+    # struct D; c::C; end
+    # jldsave("$(my_object_filename)"; b=B(A(42)), d=D(C(42)))
+
+
+    struct A; x::Float64; end
+    struct B; a::A; end
+
+    struct C; x::Tuple{Int,Int}; end
+    struct D; c::C; end
+    @testset "Recoverable changes in structs, Issue #354" begin
+        b = load(fn, "b")
+        @test b == B(A(42.0))
+
+        d = load(fn, "d")
+        # Reconstructed type has correct value
+        @test d.c.x == 42
+        # This is evil
+        #Base.convert(::Type{Tuple{Int,Int}}, x::Int) = (x, 2x)
+        #d = load(fn, "d")
+        #@test d.c.x == (42, 84)
+    end
+end
+
+
+
+module Readas
+    using JLD2
+    struct UndefinedFunction <:Function
+        fun
+    end
+    (f::UndefinedFunction)(args...; kwargs...) = error("The function \$(f.fun) is not defined")
+
+    # Case when readas is defined
+    # If `F` doesn't exist when reading, should be read as `Foo{UndefinedFunction}`
+    struct Foo{F<:Function}
+        fun::F
+    end
+    struct FooSerialization
+        fun
+    end
+    JLD2.writeas(::Type{<:Foo}) = FooSerialization
+    Base.convert(::Type{<:FooSerialization}, f::Foo) = FooSerialization(f.fun)
+
+    JLD2.readas(::Type{<:FooSerialization}) = Foo
+    function Base.convert(::Type{<:Foo}, f::FooSerialization)
+        isa(f.fun, Function) && return Foo(f.fun)
+        return Foo(UndefinedFunction(f.fun))
+    end
+
+    # Case when readas is not defined (N)
+    # If `F` doesn't exist when reading, should be read as `FooNSerialization`
+    struct FooN{F<:Function}
+        fun::F
+    end
+    struct FooNSerialization
+        fun
+    end
+    JLD2.writeas(::Type{<:FooN}) = FooNSerialization
+    Base.convert(::Type{<:FooNSerialization}, f::FooN) = FooNSerialization(f.fun)
+    Base.convert(::Type{<:FooN}, f::FooNSerialization) = FooN(f.fun)
+end
+
+# jldsave("readas_foo_sin.jld2"; foo=Readas.Foo(sin))
+# fun(x) = x^2
+# jldsave("readas_foo_a.jld2"; foo=Readas.Foo(fun))
+# jldsave("readas_foo_n_sin.jld2"; foo=Readas.FooN(sin))
+# jldsave("readas_foo_n_a.jld2"; foo=Readas.FooN(fun))
+
+@testset "readas api for struct upgrades" begin
+    getfoo(file) = jldopen(joinpath(testfiles,file)) do io; io["foo"]; end
+    @test getfoo("readas_foo_sin.jld2") isa Readas.Foo{typeof(sin)}
+    @test getfoo("readas_foo_n_sin.jld2") isa Readas.FooN{typeof(sin)}
+    @test getfoo("readas_foo_a.jld2") isa Readas.Foo{Readas.UndefinedFunction}
+    @test getfoo("readas_foo_n_a.jld2") isa Readas.FooNSerialization
 end
