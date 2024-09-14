5ed91ab1f3eaabd1ec28942bc079ff3043bba4b6
Jonas Isensee <jonas.isensee@web.de>
2024-09-14 09:39:50 +0200

    can't test for warning if warning is removed

--- test/loadsave.jl
+++ test/loadsave.jl
@@ -764,14 +764,7 @@ end
         eval(:(module ModuleWithFunction
             fun(x) = x+1
         end))
-        if VERSION ≥ v"1.7"
-            @test_warn(
-                contains("Function types cannot"),
-                eval(:(save_object("test.jld2", (1, ModuleWithFunction.fun, 2))))
-            )
-        else
-            eval(:(save_object("test.jld2", (1, ModuleWithFunction.fun, 2))))
-        end
+        eval(:(save_object("test.jld2", (1, ModuleWithFunction.fun, 2))))
         obj = load_object("test.jld2")
         @test length(obj) == 3
         @test obj[1] == 1
